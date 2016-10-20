a1b435f0481985ce75140b264cf863e7a58946c6
Peter Ginty <pginty@umail.ucsb.edu>
2016-10-20 11:49:09 -0700

    PG/TC moved source files to correct location

---
+++ src/RecipeList.java
@@ -0,0 +1,35 @@
+package edu.ucsb.cs56.S12.m_a_p.cp3;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/** RecipeList represents a list of recipes
+
+*/
+public class RecipeList extends ArrayList<Recipe> implements Serializable {
+
+	
+	/**
+    no-arg constructor makes a list with two empty slots
+	 */	
+	RecipeList(){
+		super(2); 
+	}
+	
+	/**
+    one-recipe-arg constructor makes a list with one recipe added and one slot empty 
+	 */	
+	RecipeList(Recipe r){
+		super(2);
+		add(r);
+		
+	}
+
+	/** TODO: Document and implement this method.. or not.  If not remove this. */
+	/*
+	public Recipe getRecipeByName(String recipeName) {
+	    return null; // Eventually, search through "this" and if any recipe has this name, return it.
+	}
+	*/
+	
+}
