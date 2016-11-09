73c7f8f618b008f4b7c5b8ee940b801f593f5a13
Peter Ginty <pginty@umail.ucsb.edu>
2016-11-09 14:44:11 -0800

    PG/TC completed IngredientsListTest.java

--- src/edu/ucsb/cs56/projects/misc/recipe_manager/IngredientsListTest.java
+++ src/edu/ucsb/cs56/projects/misc/recipe_manager/IngredientsListTest.java
@@ -9,9 +9,31 @@ import static org.junit.Assert.assertEquals;
  */
 
 public class IngredientsListTest {
+    
     @Test
     public void testEmptyIngredientsList() {
         IngredientsList empty = new IngredientsList();
         assertEquals(empty.size(), 0);
     }
+
+    @Test
+    public void testSingleElementIngredientsList() {
+        IngredientsList single = new IngredientsList();
+        single.add("Carrots");
+        assertEquals(single.size(), 1);
+        assertEquals(single.get(0), "Carrots");
+    }
+
+    @Test
+    public void testMultiElementIngredientsList() {
+        IngredientsList single = new IngredientsList();
+        single.add("Carrots");
+        single.add("Onions");
+        single.add("Celery");
+        assertEquals(single.size(), 3);
+        assertEquals(single.get(0), "Carrots");
+        assertEquals(single.get(1), "Onions");
+        assertEquals(single.get(2), "Celery");
+    }
+
 }
